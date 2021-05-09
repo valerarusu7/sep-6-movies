8461f1c35c5a2b7cacd19dcfae6fbfecdfe85eff
Valeriu Rusu <280167@via.dk>
2021-05-09 20:34:56 +0300

    Login Redesign

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { signIn } from "../store/reducers/authReducer";
+import style from "../styles/Login.module.css";
+import photo from "../assets/google_PNG19635.png";
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -9,10 +11,19 @@ const Login = () => {
   useEffect(() => {}, []);
 
   return (
-    <div className="Login">
-      <Link to="/">
-        <button onClick={() => dispatch(signIn())}>Login</button>
-      </Link>
+    <div className={style.container}>
+      <div className={style.login}>
+        <Link to="/" className={style.button}>
+          <div onClick={() => dispatch(signIn())}>Login with Google</div>
+        </Link>
+      </div>
+      <div className={style.drops}>
+        <div className={style.drop1}></div>
+        <div className={style.drop2}></div>
+        <div className={style.drop3}></div>
+        <div className={style.drop4}></div>
+        <div className={style.drop5}></div>
+      </div>
     </div>
   );
 };
