ab018b3ddf81aa4b459decae195c8f40ec8b394e
ViktoriaKouni <43205496+ViktoriaKouni@users.noreply.github.com>
2021-05-19 01:54:06 +0200

    data is being displayed
    design to be continued

--- src/components/Actor.js
+++ src/components/Actor.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { getActorById } from "../store/reducers/personReducer.js";
-import styles from "../styles/Movie.module.css";
+import styles from "../styles/Person.module.css";
 
 const Actor = () => {
   const { actor} = useSelector(
     (state) => state.person,
   );
+  const actorLoading = useSelector((state) => state.person.loading);
+
   const dispatch = useDispatch();
   const { id } = useParams();
 
@@ -19,11 +21,25 @@ const Actor = () => {
 {console.log(actor)}
  
   return (
-    <div>
-      <div style={{ backgroundColor: "#44014C", width: "300px", minHeight: "200px"}}>{actor != null ? actor.name : 'SUKA' }</div>
-      <p>WORK PLZ</p>
+    <div className={styles.page}>
+        {actorLoading !=true ? (
+            <div>
+                {actor != undefined || null ? (
+                <div className={styles.right}>
+                    
+                    <img className={styles.img}
+                     src={`https://image.tmdb.org/t/p/original/${actor.details.profile_path}`}
+                     alt={actor.details.id}/>
+                                  <div className={styles.left}>{actor.details.biography}</div>
+                </div>
+                ): null}
+      
+            </div>) : null}
+            
+            {console.log(actor)}
 </div>
   );
 };
+
 
 export default Actor;
