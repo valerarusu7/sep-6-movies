a1b3dad1dda74fd3de1cc5ed6669de8712c5d895
Valeriu Rusu <280167@via.dk>
2021-06-01 17:09:29 +0300

    icon added

--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -2,9 +2,11 @@ import React, { useEffect } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import style from "../../styles/Main.module.css";
 const Layout = ({ children }) => {
+
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, []);
+  
   return (
     <div className={{ display: "flex", flexDirection: "column" }}>
       <div className={{ display: "flex", flexGrow: 1 }}>
